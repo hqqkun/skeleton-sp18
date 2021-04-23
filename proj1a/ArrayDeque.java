545356b5a69741b335aa4106191e0793ae5c758c
阿昆 <37210982+hqqkun@users.noreply.github.com>
2021-04-23 14:01:52 +0800

    Add files via upload

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,122 @@
+public class ArrayDeque<T> {
+    private T[] data;
+    private int front;
+    private int rear;
+    private int maxSize;
+
+    public ArrayDeque() {
+        data = (T[]) new Object[8];
+        maxSize = 8;
+        front = rear = 0;
+    }
+
+    public int size() {
+        return (rear - front + maxSize) % maxSize;
+    }
+
+    public boolean isEmpty() {
+        return front == rear;
+    }
+
+    private boolean isFull() {
+        return addOne(rear) == front;
+    }
+
+    private int minusOne(int index){
+        return (index -1 + maxSize) % maxSize;
+    }
+
+    private int addOne(int index){
+        return (index + 1) % maxSize;
+    }
+
+    private int getProperSize(int dir){
+        if(dir == 1){
+            return maxSize << 1;
+        }
+        // goes down
+        int sentinalSize = size() << 2;
+        int newSize = maxSize;
+        for(; newSize > sentinalSize; newSize >>= 1)
+            ;
+        return newSize;
+    }
+
+    private void reshape(int newSize) {
+        int size = size();
+        T[] tmp = (T[]) new Object[newSize];
+        System.arraycopy(data, (front + 1) % maxSize, tmp, 1, maxSize - 1 - front);
+        System.arraycopy(data, 0, tmp, maxSize - front, (rear + 1) % maxSize);
+        data = tmp;
+        front = 0;
+        rear = size;
+        maxSize = newSize;
+    }
+
+    private void cheakUsage() {
+        int size = size();
+        if(size >= 16 && ((size<<2)< maxSize)){
+            reshape(getProperSize(0));
+        }
+    }
+
+    public void addLast(T item) {
+        if (isFull()) {
+            reshape((getProperSize(1)));
+        }
+        rear = addOne(rear);
+        data[rear] = item;
+    }
+
+    public void addFirst(T item) {
+        if (isFull()) {
+            reshape((getProperSize(1)));
+        }
+        data[front] = item;
+        front = minusOne(front);
+    }
+
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        front = addOne(front);
+        T tmp = data[front];
+        data[front] = null;
+        cheakUsage();
+        return tmp;
+    }
+
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        T tmp = data[rear];
+        data[rear] = null;
+        rear = minusOne(rear);
+        cheakUsage();
+        return tmp;
+    }
+
+    public void printDeque(){
+        if(isEmpty())
+            return;
+        int ptr = addOne(front);
+        int end = addOne(rear);
+        for(; ptr != end; ptr = addOne(ptr)){
+            System.out.print(data[ptr] + " ");
+        }
+        System.out.print('\n');
+    }
+
+    public T get(int index){
+        if((index + 1) > size()){
+            return null;
+        }
+        int i = 0;
+        int ptr = addOne(front);
+        for(; i != index; ptr = addOne(ptr))
+            ++i;
+        return data[ptr];
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,107 @@
+public class LinkedListDeque<T> {
+
+    private class Node {
+        public Node prev;
+        public T item;
+        public Node next;
+
+        public Node(T item, Node prev, Node next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        public T helper(int needIndex,int myIndex){
+            if(needIndex == myIndex)
+                return item;
+            return next.helper(needIndex,myIndex + 1);
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    public LinkedListDeque(){
+        size = 0;
+        head = new Node(null,null,null);
+        head.next = head;
+        head.prev = head;
+    }
+
+    /** Returns the number of items in the deque. */
+    public int size(){
+        return size;
+    }
+
+    /**  Returns true if deque is empty, false otherwise. */
+    public boolean isEmpty(){
+        return head.next == head;
+    }
+
+    /** Adds an item of type T to the front of the deque. */
+    public void addFirst(T item){
+        ++size;
+        Node tmp = new Node(item,head,head.next);
+        head.next.prev = tmp;
+        head.next = tmp;
+    }
+
+    /** Removes and returns the item at the front of the deque. If no such item exists, returns null. */
+    public T removeFirst(){
+        if(isEmpty()){
+            return null;
+        }
+        Node tmp = head.next;
+        head.next = tmp.next;
+        tmp.next.prev = head;
+        --size;
+        return tmp.item;
+    }
+
+    /**  Adds an item of type T to the back of the deque. */
+    public void addLast(T item){
+        ++size;
+        Node tmp = new Node(item,head.prev,head);
+        head.prev.next = tmp;
+        head.prev = tmp;
+    }
+
+    /** Removes and returns the item at the back of the deque. If no such item exists, returns null. */
+    public T removeLast(){
+        if(isEmpty()){
+            return null;
+        }
+        --size;
+        Node tmp = head.prev;
+        head.prev = tmp.prev;
+        tmp.prev.next = head;
+        return tmp.item;
+    }
+
+    public T get(int index){
+        if( (index + 1) > size)
+            return null;
+        int i = 0;
+        Node ptr = head.next;
+        for( ; i != index; ptr = ptr.next)
+            ++i;
+        return ptr.item;
+    }
+    
+    public T getRecursive(int index){
+        if((index + 1) > size)
+            return null;
+        return head.next.helper(index,0);
+    }
+
+    /** Prints the items in the deque from first to last, separated by a space. */
+    public void printDeque(){
+        if(size == 0)
+            return;
+        for(Node ptr = head.next; ptr != head;ptr = ptr.next){
+            System.out.print(ptr.item + " ");
+        }
+        System.out.print('\n');
+    }
+
+}
