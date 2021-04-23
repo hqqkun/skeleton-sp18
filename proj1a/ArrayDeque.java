259de86bb8fa606c3c753c524443d31383eb8ed1
阿昆 <37210982+hqqkun@users.noreply.github.com>
2021-04-23 14:12:04 +0800

    Add files via upload

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -9,27 +9,21 @@ public class ArrayDeque<T> {
         maxSize = 8;
         front = rear = 0;
     }
-
     public int size() {
         return (rear - front + maxSize) % maxSize;
     }
-
     public boolean isEmpty() {
         return front == rear;
     }
-
     private boolean isFull() {
         return addOne(rear) == front;
     }
-
     private int minusOne(int index){
         return (index -1 + maxSize) % maxSize;
     }
-
     private int addOne(int index){
         return (index + 1) % maxSize;
     }
-
     private int getProperSize(int dir){
         if(dir == 1){
             return maxSize << 1;
@@ -41,25 +35,29 @@ public class ArrayDeque<T> {
             ;
         return newSize;
     }
-
     private void reshape(int newSize) {
         int size = size();
         T[] tmp = (T[]) new Object[newSize];
-        System.arraycopy(data, (front + 1) % maxSize, tmp, 1, maxSize - 1 - front);
-        System.arraycopy(data, 0, tmp, maxSize - front, (rear + 1) % maxSize);
+//        System.arraycopy(data, (front + 1) % maxSize, tmp, 1, maxSize - 1 - front);
+//        System.arraycopy(data, 0, tmp, maxSize - front, (rear + 1) % maxSize);
+        int newFront = 0;
+        int end = addOne(rear);
+        int ptr = addOne(front);
+        for(;ptr != end;ptr = addOne(ptr)){
+            tmp[newFront] = data[ptr];
+            newFront = addOne(newFront);
+        }
         data = tmp;
         front = 0;
         rear = size;
         maxSize = newSize;
     }
-
     private void cheakUsage() {
         int size = size();
         if(size >= 16 && ((size<<2)< maxSize)){
             reshape(getProperSize(0));
         }
     }
-
     public void addLast(T item) {
         if (isFull()) {
             reshape((getProperSize(1)));
@@ -67,7 +65,6 @@ public class ArrayDeque<T> {
         rear = addOne(rear);
         data[rear] = item;
     }
-
     public void addFirst(T item) {
         if (isFull()) {
             reshape((getProperSize(1)));
@@ -75,7 +72,6 @@ public class ArrayDeque<T> {
         data[front] = item;
         front = minusOne(front);
     }
-
     public T removeFirst() {
         if (isEmpty()) {
             return null;
@@ -86,7 +82,6 @@ public class ArrayDeque<T> {
         cheakUsage();
         return tmp;
     }
-
     public T removeLast() {
         if (isEmpty()) {
             return null;
@@ -97,7 +92,6 @@ public class ArrayDeque<T> {
         cheakUsage();
         return tmp;
     }
-
     public void printDeque(){
         if(isEmpty())
             return;
@@ -108,7 +102,6 @@ public class ArrayDeque<T> {
         }
         System.out.print('\n');
     }
-
     public T get(int index){
         if((index + 1) > size()){
             return null;
