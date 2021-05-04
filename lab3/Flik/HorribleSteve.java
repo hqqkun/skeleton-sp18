30a85f2e2ff25632dca320d144e82a37696e9221
阿昆 <37210982+hqqkun@users.noreply.github.com>
2021-05-04 18:04:20 +0800

    Update HorribleSteve.java

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,7 +1,9 @@
 public class HorribleSteve {
     public static void main(String [] args) {
         int i = 0;
-        for (int j = 0; i < 500; ++i, ++j) {
+        int j;
+        for ( i = 0; i < 500; ++i) {
+            j = i;
             if (!Flik.isSameNumber(i, j)) {
                 break; // break exits the for loop!
             }
